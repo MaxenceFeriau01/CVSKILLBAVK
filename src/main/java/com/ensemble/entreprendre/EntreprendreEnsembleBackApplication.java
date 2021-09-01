48ba0ed3d63bfde55087bf4ff921fb68ba60fdd2
Ammar ZEBBAR <ammar.zebbar@modiscloud.net>
2021-09-01 11:52:05 +0200

    init springboot

---
+++ src/main/java/com/ensemble/entreprendre/EntreprendreEnsembleBackApplication.java
@@ -0,0 +1,13 @@
+package com.ensemble.entreprendre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntreprendreEnsembleBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntreprendreEnsembleBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensemble/entreprendre/EntreprendreEnsembleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensemble.entreprendre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntreprendreEnsembleBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

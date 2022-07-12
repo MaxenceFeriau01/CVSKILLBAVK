7b765fb1d13aac40d1592663886c74503b549af4
Adrien HOLVOET <adrien.holvoet@modiscloud.net>
2022-07-12 17:21:30 +0200

    update springboot/swagger version for vulnerabilityu + add depency for hot code replace

--- src/main/java/com/ensemble/entreprendre/config/SwaggerConfig.java
+++ src/main/java/com/ensemble/entreprendre/config/SwaggerConfig.java
@@ -16,33 +16,37 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+//http://localhost:8080/swagger-ui/
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
 	@Value("${swagger.context.path:/}")
 	private String context;
 
-	 @Bean
+	@Bean
 	public Docket api() {
-	return new Docket(DocumentationType.SWAGGER_2).pathMapping(this.context)
-	.securitySchemes(Collections.singletonList(this.apiKey()))
-	.securityContexts(Collections.singletonList(this.securityContext()))
-	.select()
-	.apis(RequestHandlerSelectors.basePackage("com.ensemble.entreprendre")).paths(PathSelectors.any()).build();
+
+		return new Docket(DocumentationType.SWAGGER_2).pathMapping(this.context)
+				.securitySchemes(Collections.singletonList(this.apiKey()))
+				.securityContexts(Collections.singletonList(this.securityContext()))
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(
+						"com.ensemble.entreprendre"))
+				.paths(PathSelectors.any())
+				.build();
 	}
 
-	 private ApiKey apiKey() {
-		 return new ApiKey("apiKey", "Authorization", "header");
+	private ApiKey apiKey() {
+		return new ApiKey("apiKey", "Authorization", "header");
 	}
 
-	 private SecurityContext securityContext() {
-		 return SecurityContext.builder().securityReferences(this.defaultAuth()).forPaths(PathSelectors.any()).build();
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(this.defaultAuth())
+				.operationSelector(operationContext -> true).build();
 	}
 
-	 private List<SecurityReference> defaultAuth() {
+	private List<SecurityReference> defaultAuth() {
 		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
 		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
 		authorizationScopes[0] = authorizationScope;

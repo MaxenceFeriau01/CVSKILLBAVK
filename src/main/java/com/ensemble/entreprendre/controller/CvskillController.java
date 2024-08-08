e9aea137c9ef59fce0ea94cc637758fea7bcb920
MaxenceFERIAU00 <maxence.feriau@akkodis.com>
2024-08-08 15:03:53 +0200

    Commencement du back

---
+++ src/main/java/com/ensemble/entreprendre/controller/CvskillController.java
@@ -0,0 +1,11 @@
+package com.ensemble.entreprendre.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(path = "/api/cvskills")
+
+public class CvskillController {
+
+
+    
+}
